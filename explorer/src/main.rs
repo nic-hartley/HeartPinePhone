29fe201a5de7e79bac2fb37d3189d190d628b3a4
Nic Hartley <nic@cybers.eco>
2020-09-21 16:59:06 -0400

    explorer: feat: poke some registers

--- explorer/src/main.rs
+++ explorer/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, explorer!");
+#![feature(lang_items, start)]
+#![no_main]
+#![no_std]
+
+#[panic_handler]
+fn on_panic(_info: &core::panic::PanicInfo) -> ! {
+  loop { }
+}
+
+#[no_mangle]
+fn _start() {
+  unsafe {
+    *(0x1c20874 as *mut u32) = 0x77711177;
+    *(0x1c2087c as *mut u32) = 0x000e0000;
+    loop { }
+  }
 }
